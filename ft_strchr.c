c81d687860bb434a4e67a491d18e8da23ac57ebf
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-11 13:23:44 +0100

    modify

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 20:14:06 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/04 11:15:17 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/06 02:58:59 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (s[i] == c)
+	if (s[i] == (char)c)
 		return ((char *)&s[i]);
 	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 20:48:07 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/04 12:43:47 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/06 02:57:58 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ char	*ft_strrchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if (c == s[i])
+		if ((char)c == s[i])
 			keep = &s[i];
 		i++;
 	}
-	if (c == s[i])
+	if ((char)c == s[i])
 		keep = &s[i];
 	return ((char *)keep);
 }
